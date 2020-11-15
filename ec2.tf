069af0981eb6b61a846f23a84d70553c5742e6e1
Fernando Sanchez <ferasanchez@edenor.com>
2020-11-15 01:44:58 -0300

    Se arreglaron varias lineas

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "TerraCloud"{
-    ami = "ami-0947d2ba12ee1ff75"
+    ami           = "ami-0947d2ba12ee1ff75"
     instance_type = "t2.micro"
     tags = {
         Name = "TerraCloud"
